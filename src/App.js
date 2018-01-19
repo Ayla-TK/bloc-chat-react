4e040c66e8656678d84e65601fc5fd28f4c7f618
Ayla Tolosa-Kline <AylaMPH@gmail.com>
2018-01-19 09:49:26 +0200

    CP5 - BlocChat React: ALMOST COMPLETE

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './styles/App.css';
 import firebase from './firebase.js';
 import { RoomList } from './components/RoomList.js';
 import { MessageList } from './components/MessageList.js';
-
+import { User } from './components/User.js';
 
 
 
@@ -12,12 +12,17 @@ import { MessageList } from './components/MessageList.js';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {activeRoom: ""};
+    this.state = {activeRoom: "", user: null};
     this.activeRoom = this.activeRoom.bind(this);
+    this.setUser = this.setUser.bind(this);
   }
 
 activeRoom(room) {
   this.setState({ activeRoom: room })
+}
+
+setUser(user) {
+   this.setState({ user: user });
 }
 
   render() {
@@ -25,6 +30,7 @@ activeRoom(room) {
     return (
       <div>
         <h1>{this.state.activeRoom.name || "Select A Room"}</h1>
+        <User firebase={firebase} setUser={this.setUser} />
         <RoomList firebase={firebase} activeRoom={this.activeRoom} />
         { showMessages ?
         (<MessageList firebase={firebase} activeRoom={this.state.activeRoom.key}/>)
